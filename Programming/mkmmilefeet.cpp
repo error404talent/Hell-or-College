cd6ce64403382ead8e5f33d79ad1bfdbee8fe62d
error404talent <nfanatalent@gmail.com>
2025-10-27 22:53:55 +0300

    d

--- Programming/mkmmilefeet.cpp
+++ Programming/mkmmilefeet.cpp
@@ -7,10 +7,55 @@ using namespace std;
 int main(){
 
     double km, m;
+    int choice;
 
     cout << "Please choose an option: " << endl;
     cout << "1. from Km" << endl;
     cout << "2. rom M " << endl;
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1 :
+        
+        cout << "Please enter distance in KM: " << endl;
+        cin >> km; 
+
+        cout << "Convert to : " << endl;
+        cout << "1. Miles " << endl;
+        cout << "Foot " << endl;
+        
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            /* code */
+            break;
+
+         case 2:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+
+        
+        
+      
+        break;
+
+    case 2 :
+     
+        break;    
+    
+    
+    
+    
+        default:
+        break;
+    }
 
     return 0;
 
