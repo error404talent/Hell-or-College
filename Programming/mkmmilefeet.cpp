98578b315b025be1d52b5de68d49c25d1c45e2c5
error404talent <nfanatalent@gmail.com>
2025-10-27 22:44:22 +0300

    g

---
+++ Programming/mkmmilefeet.cpp
@@ -0,0 +1,19 @@
+// Задача 7 — Перевод расстояния из километров и метров в мили и футы
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    double km, m;
+
+    cout << "Please choose an option: " << endl;
+    cout << "1. from Km" << endl;
+    cout << "2. rom M " << endl;
+
+    return 0;
+
+
+}
+
