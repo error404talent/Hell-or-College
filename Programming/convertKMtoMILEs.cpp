86f8490fb972a0d70435287d4c35032453fb849f
error404talent <nfanatalent@gmail.com>
2025-10-27 22:29:51 +0300

    Pracyice session 3; Kilometre to mile conversion

--- Programming/convertKMtoMILEs.cpp
+++ Programming/convertKMtoMILEs.cpp
@@ -1,12 +1,20 @@
 // Задача 6 — Перевод километров в мил
 
 #include<iostream>
+using namespace std;
+
 
 int main(){
 
     double kilometres, miles;
 
-    cout << "Please in distance(KM)";
+    cout << "Please in distance(KM):  ";
+    cin >> kilometres;
 
-    
+    miles = kilometres * 0.621371;
+
+    cout << kilometres << "Kilometres = "  << miles << "miles"  << endl;
+
+    return 0;
+
 }
