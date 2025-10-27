bcb436f237a1f931344be5543bb033c6fffd16fc
error404talent <nfanatalent@gmail.com>
2025-10-27 22:20:24 +0300

    cooments on task main focus

--- Programming/firstfiveoddnumbers.cpp
+++ Programming/firstfiveoddnumbers.cpp
@@ -1,3 +1,5 @@
+//Задача 2 — Отобразить первые пять нечётных чисел
+
 #include<iostream>
 using namespace std;
 
--- Programming/naturalnumbers.cpp
+++ Programming/naturalnumbers.cpp
@@ -1,3 +1,5 @@
+// Задача 1 — Вывести натуральные числа от 1 до 10
+
 #include<iostream>
 using namespace std;
 
