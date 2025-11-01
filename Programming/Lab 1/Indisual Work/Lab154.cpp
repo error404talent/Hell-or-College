75f1f4e17b5ce7b703555c0f455395cc49d66f60
error404talent <nfanatalent@gmail.com>
2025-11-01 20:31:02 +0300

    Indivisual work lab 1

---
+++ Programming/Lab 1/Indisual Work/Lab154.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdio.h>
+
+int main(){
+
+    int N,M;
+
+    printf("EEnter the number of rows: " );
+    scanf("%d", &N);
+
+    printf("enter the number of columms;  ");
+    scanf("%d", &M);
+
+    
+
+
+
+
+
+}
